209937eb6a9046d987400fc1cf69b9fd06b9b1bb
LoranceChen <wangchen@qiaobutang.com>
2017-01-10 22:23:31 +0800

    refine

--- Assets/DrawLine.cs
+++ Assets/DrawLine.cs
@@ -15,27 +15,20 @@ public class DrawLine : MonoBehaviour {
 	void Start () {
 		VectorManager.useDraw3D = true;
 
-
 		var pos3d = transf.Select (x => x.position).ToList();
 		var pos2d = pos3d.Select(x => new Vector2(x.x, x.y)).ToList();
+
+		//draw 2d
 		vobj.vectorLine.Resize (pos2d.Count + 1);
 		vobj.vectorLine.points2 = pos2d;
 		vobj.vectorLine.points3 = pos3d;
-
-
 		vobj.vectorLine.Draw ();
-		var vl = new VectorLine ("a", pos3d, 100f);
-		vl.Draw3DAuto ();
 
+		//draw 3d
+		var vl = new VectorLine ("a", pos3d, 100f);
 		vobj3d.SetVectorLine (vl, vobj.texture, mat);
 		vl.points3.ForEach (x => Debug.Log(x));
-		vobj3d.UpdateVerts ();
-		vobj3d.UpdateUVs ();
-		vobj3d.UpdateTris ();
-//		vobj3d.UpdateTangents ();
-//		vobj3d.UpdateNormals ();
-//		vobj3d.UpdateMeshAttributes ();
-//		vobj3d.UpdateColors ();
+		vl.Draw3DAuto ();
 	}
 
 }
